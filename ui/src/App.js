b01c27d766aaa507042aff4b22340a38100f1cf9
arunkumarsilla <arunkumars@schemaxtech.com>
2024-04-27 21:32:22 +0530

    create_react_nodejs

---
+++ ui/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [userName, setUsername] = useState('');
+
+  useEffect(() => {
+    getName();
+  }, []);
+
+  const getName = async () => {
+    try {
+      const response = await axios.get('/names');
+      console.log(response.data);
+      setUsername(response.data);
+    } catch (error) {
+      console.error('Error fetching names:', error);
+    }
+  };
+
+  return (
+    <>
+    <h1>My application</h1>
+    <h3>{userName}</h3>
+    
+    </>
+      
+    
+  );
+}
+
+export default App;
